13db00886b3abad841417c41f5191d0e9d61b643
chidvipemula <chidvi.pemula@gmail.com>
2019-01-24 20:51:52 -0700

    testing git hub integration

--- DatabaseAnalysis.py
+++ DatabaseAnalysis.py
@@ -2,10 +2,10 @@ import GetClient
 client = GetClient.returnClient()
 query = ("""SELECT count(*) FROM `infusionsoft-looker-poc.asu_msba_customer_ltv.CONFIDENTIAL_ltv_oppty_table` LIMIT 10""")
 query_job = client.query(query)  # API request - starts the query
-Total_Records = query_job.result()[0][0]
+Obj_Total_Records = query_job.result()
 print()
 print("======= Printing total records ==========")
-print(Total_Records)
+print(Obj_Total_Records)
 print()
 print("======= Printed total records ==========")
 # table = client.get_table('infusionsoft-looker-poc.asu_msba_customer_ltv.CONFIDENTIAL_ltv_oppty_table')
